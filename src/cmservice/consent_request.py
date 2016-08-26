4b0248338634c7c651705e1a0420d15535ded352
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-08-26 06:33:03 +0200

    Fix ConsentRequest constructor.

--- src/cmservice/consent_request.py
+++ src/cmservice/consent_request.py
@@ -4,13 +4,13 @@ from datetime import datetime, timedelta
 class ConsentRequest(object):
     def __init__(self, data: dict, timestamp: datetime = None):
         """
-        :param data: information needed to show the consent page
-        :param timestamp: when the ticket data object was created
+        :param data: params in the consent request, will be displayed on the consent page
+        :param timestamp: when the request object was created
         """
-        mandatory_params = set(['id', 'attr', 'redirect_endpoint'])
+        mandatory_params = {'id', 'attr', 'redirect_endpoint'}
         if not mandatory_params.issubset(set(data.keys())):
             # missing required info
-            raise ValueError('Incorrect consent request, missing some of mandatory params'.format(mandatory_params))
+            raise ValueError('Incorrect consent request, missing some mandatory params'.format(mandatory_params))
 
         if not timestamp:
             timestamp = datetime.now()
